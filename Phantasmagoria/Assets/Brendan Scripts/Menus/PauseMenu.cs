c34a3f01548aa5691386e6d8678616c1280e73c9
chanbrendan62-debug <chanbrendan62@gmail.com>
2026-07-14 19:14:34 -0400

    added pause menu
    added pause menu

--- Phantasmagoria/Assets/Brendan Scripts/Menus/PauseMenu.cs
+++ Phantasmagoria/Assets/Brendan Scripts/Menus/PauseMenu.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject pauseMenuUI;
 
-    // Update is called once per frame
+    private bool isPaused = false;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
