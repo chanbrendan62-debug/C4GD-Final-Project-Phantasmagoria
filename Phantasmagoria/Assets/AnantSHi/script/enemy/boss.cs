109421133b9f8188cb4fbba20ee02a4437def3bd
CuzinBR <anantpazsingh@gmail.com>
2026-07-16 14:58:39 -0500

    ALL enemies with animations and shiiiiii

--- Phantasmagoria/Assets/AnantSHi/script/enemy/boss.cs
+++ Phantasmagoria/Assets/AnantSHi/script/enemy/boss.cs
@@ -5,35 +5,95 @@ using UnityEngine.U2D;
 
 public class boss : ENEMYBASE
 {
-
     public float biggerRange;
 
+    // projectile instantiate
+    public GameObject projectile;
+    public float projSpeed;
+    public float projDamage;
+    public float projKnockback;
 
+    // cooldowns
+    public float projCooldown;
+    float currentCooldown;
+
+    public bool damaging = false;
+
+    public override void Start()
+    {
+        base.Start();
+        health.OnDamaged += Damaged;
+    }
 
     public override void Idle()
     {
+        if (damaging) { return; }
+
         base.Idle();
+        currentCooldown = projCooldown;
         anim.SetBool("chasing", false);
+
         Transform sprite = anim.transform;
         sprite.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        // OPTIONAL: Stop horizontal movement when idling, while letting gravity do its thing
+        rb.velocity = new Vector2(0, rb.velocity.y);
     }
 
     public override void Chase()
     {
+        if (damaging) { return; }
 
         chaseRange = biggerRange;
-
         anim.SetBool("chasing", true);
         Transform sprite = anim.transform;
 
         float diff = PlayerInstance.Instance.transform.position.x - transform.position.x;
         int dir = (int)Mathf.Sign(diff);
 
-        sprite.transform.rotation = Quaternion.Euler(0,0, 12.76f * -dir);
+        sprite.transform.rotation = Quaternion.Euler(0, 0, 12.76f * -dir);
 
-        rb.MovePosition(new Vector2(rb.position.x + speed * dir * Time.fixedDeltaTime, rb.position.y));
+        // FIXED: Set the velocity directly, preserving gravity (rb.velocity.y)
+        rb.velocity = new Vector2(speed * dir, rb.velocity.y);
 
+        // projectile Timer
+        if (currentCooldown < 0)
+        {
+            projtile(dir);
+            currentCooldown = projCooldown;
+        }
+        currentCooldown -= Time.deltaTime;
     }
+
+    void projtile(int dir)
+    {
+        GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
+        projectile ctile = proj.GetComponent<projectile>();
+        ctile.dmg = projDamage;
+        ctile.knockback = projKnockback;
+        ctile.speed = projSpeed;
+
+        ctile.dir = new Vector2(dir, 0);
+    }
+
+    void Damaged(float damage)
+    {
+        damaging = true;
+        anim.SetBool("hit", true);
+        StartCoroutine(Knockback());
+    }
+
+    IEnumerator Knockback()
+    {
+        yield return new WaitForSeconds(health.iFrames);
+
+        
+
+        anim.SetBool("hit", false);
+        damaging = false;
+    }
+
+
 
 
 }
