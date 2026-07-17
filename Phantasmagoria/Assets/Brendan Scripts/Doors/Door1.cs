e4013621317c1b908e7d96a35bb72ac28452e530
chanbrendan62-debug <chanbrendan62@gmail.com>
2026-07-16 22:41:15 -0400

    e
    e

--- Phantasmagoria/Assets/Brendan Scripts/Doors/Door1.cs
+++ Phantasmagoria/Assets/Brendan Scripts/Doors/Door1.cs
@@ -1,37 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door1 : MonoBehaviour
 {
     public Animator animator;
-    // Start is called before the first frame update
+    public GameObject text;
+    private bool playerInRange = false;
+    private bool doorAnimationFinished = false;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange && doorAnimationFinished && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Room2Scene");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            animator.SetTrigger("DoorOpener");      
+            if (animator != null) animator.SetTrigger("DoorOpener");
+            playerInRange = true;
+
+            if (doorAnimationFinished && text != null)
+            {
+                text.SetActive(true);
+            }
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+            if (text != null)
+            {
+                text.SetActive(false);
+            }
+        }
+    }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-        //if (collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
-       // {   
-            //  SceneManager.LoadScene("");
-        //}
-    //}
+    public void OnDoorAnimationFinished()
+    {
+        doorAnimationFinished = true;
+
+        if (playerInRange && text != null)
+        {
+            text.SetActive(true);
+        }
+    }
 }
--- Phantasmagoria/Assets/Brendan Scripts/TutorialSign.cs
+++ Phantasmagoria/Assets/Brendan Scripts/TutorialSign.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class TutorialSign : MonoBehaviour
-{
-    public GameObject text;
-    // Start is called before the first frame update
-    void Start()
+    public class TutorialSign : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        public GameObject text;
+        // Start is called before the first frame update
+        void Start()
         {
-            text.SetActive(true);
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                text.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                text.SetActive(false);
+            }
         }
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            text.SetActive(false);
-        }
-    }
-}
