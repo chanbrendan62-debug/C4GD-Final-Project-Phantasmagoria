e92cbe2d466128cfd3a60c86354bb128ffa61862
chanbrendan62-debug <chanbrendan62@gmail.com>
2026-07-17 02:22:00 -0400

    e
    e

---
+++ Phantasmagoria/Assets/Brendan Scripts/Doors/Door2.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door2 : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject text;
+    private bool playerInRange = false;
+    private bool doorAnimationFinished = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerInRange && doorAnimationFinished && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Room3Scene");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (animator != null) animator.SetTrigger("DoorOpener");
+            playerInRange = true;
+
+            if (doorAnimationFinished && text != null)
+            {
+                text.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+            if (text != null)
+            {
+                text.SetActive(false);
+            }
+        }
+    }
+
+    public void OnDoorAnimationFinished()
+    {
+        doorAnimationFinished = true;
+
+        if (playerInRange && text != null)
+        {
+            text.SetActive(true);
+        }
+    }
+}
